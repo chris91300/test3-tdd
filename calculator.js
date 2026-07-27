aec42e418737110f943baec9273eda14271c8422
MICHEL Christophe <christophe.michel@externe.maif.fr>
2026-07-27 15:15:40 +0200

    update add with possibility to change the delimiter

--- calculator.js
+++ calculator.js
@@ -1,9 +1,21 @@
 
+const changeDelimiterRegex = /^\/\/(.+)\n(.*)/s;
+
 export default function add(text) {
     if (test === "") {
         return 0;
     }
 
-    const numbers = text.replaceAll("\n", ",").split(",");
-    return numbers.reduce((total, number) => total + Number(number), 0);
+    if (changeDelimiterRegex.test(text)) {
+        const utils = text.match(changeDelimiterRegex);
+        const delimiter = utils[1];
+        const numbers = utils[2];
+        const numbersSplitted = numbers.split(delimiter);
+        return numbersSplitted.reduce((total, number) => total + Number(number), 0);
+    } else {
+        const numbers = text.replaceAll("\n", ",").split(",");
+        return numbers.reduce((total, number) => total + Number(number), 0);
+    }
+
+
 }
